ce7933d95faa8b757f4fd0b5950b5e08783b1739
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-03-24 14:59:42 -0500

    chore: initial commit, setup compose and properties

---
+++ src/main/java/pe/com/carlosh/tallyapi/TallyApiApplication.java
@@ -0,0 +1,13 @@
+package pe.com.carlosh.tallyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TallyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/com/carlosh/tallyapi/TallyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.carlosh.tallyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
